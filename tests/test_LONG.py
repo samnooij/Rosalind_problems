7f743f3dbd3ba520dc70c1f6059ae0d1e866e7e9
Sam Nooij <samnooij@protonmail.com>
2021-05-03 16:16:56 +0200

    Add solution to test cases

--- tests/test_LONG.py
+++ tests/test_LONG.py
@@ -29,3 +29,15 @@ def test_read_sequences():
 def test_find_overlaps():
     # Make sure the sample dataset works as intended
     assert find_overlaps(sample_dictionary) == "ATTAGACCTGCCGGAATAC"
+
+
+def test_rosalind_exercise():
+    # Make sure that the script produces the right answer as judged by Rosalind
+    test_file = "data/rosalind_long5.txt"
+    answer_file = "results/long5.txt"
+
+    with open(answer_file, "r") as read_file:
+        answer = read_file.readline().strip()
+
+    sequence_dictionary = read_sequences(test_file)
+    assert find_overlaps(sequence_dictionary) == answer
