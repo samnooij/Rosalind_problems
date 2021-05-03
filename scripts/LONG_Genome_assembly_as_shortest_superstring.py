8b32eb9e17252128947edd66c684e865b60f481a
Sam Nooij <samnooij@protonmail.com>
2021-05-03 17:16:13 +0200

    Remove note to self comments

--- scripts/LONG_Genome_assembly_as_shortest_superstring.py
+++ scripts/LONG_Genome_assembly_as_shortest_superstring.py
@@ -330,20 +330,6 @@ contig : contiguous sequence
     # ...and start from (1)
     return find_overlaps(seq_dict_copy)
 
-    # When finished, include test cases that also show how the script works:
-    # for example, start by matching sequences 'A' and 'A', then 'AC' and 'CG',
-    # add in some interesting but simple cases and slowly increase complexity.
-    #
-    # Also, when this script works, I would like to implement a version that
-    # takes into account reverse complements of sequences as well.
-    # (Not particularly useful in this exercise, but may come in handy some time...)
-    # After that, it may be nice to add a version that does not require
-    # all sequences to have at least some overlap and just returns multiple
-    # sequences if some sequences share no overlapping parts.
-    # (This should probably also use a cut-off for overlap length:
-    # overlaps of length 1 are not that hard to find, but are often meaningless
-    # when applied to actual genomics data.)
-
 
 def main():
     """
@@ -376,15 +362,3 @@ def main():
 
 if __name__ == "__main__":
     exit(main())
-
-## Minimum required features:
-# 1. Compare ends of sequences, long to short
-#  --> merge to first = longest match, there should be only one
-# 2.
-#
-## Bonus features for extended usability:
-# 1. Collapse all duplicates
-#  --> include duplicates that are subsequences of longer sequences
-# 2. Continue searching after an end has been matched: there may be multiple options
-#  --> in case of multiple options, try all or stop extending?
-# 3. Include reverse complements as option
